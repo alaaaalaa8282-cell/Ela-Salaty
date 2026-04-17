5f941340bb93b888b27003bcc12f564d7267e3a2
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-04-17 13:21:47 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,8 +17,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
-rootProject.name = "Ela-Salatye"
+rootProject.name = "Ela-Salaty"
 include(":app")
